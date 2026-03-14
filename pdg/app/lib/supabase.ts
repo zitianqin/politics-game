7ba5473f5e554b1a23a94d3a67f293062526cd8e
Danar Prakosa <danarprakosa@gmail.com>
2026-03-14 14:08:47 +1100

    1.1 add db schema

--- pdg/app/lib/supabase.ts
+++ pdg/app/lib/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "./database.types";
 
 export function getSupabaseClient() {
-  return createClient(
+  return createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
